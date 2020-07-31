e6e088d9559f85837a09c1ff6fc8091db1e1a1a8
Delaney <dlothian@ualberta.ca>
2020-07-30 21:55:38 -0600

    inital dump

---
+++ LSAMichif/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatListModule } from '@angular/material/list';
+import { CountingComponent } from './components/counting/counting.component';
+import { MoveComponent } from './components/move/move.component';
+import { SharingComponent } from './components/sharing/sharing.component';
+import { StoriesComponent } from './components/stories/stories.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MainNavComponent,
+    CountingComponent,
+    MoveComponent,
+    SharingComponent,
+    StoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatIconModule,
+    MatSidenavModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatButtonModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
